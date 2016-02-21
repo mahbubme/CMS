6af0aea59bc66def9d5107bc703ff02f21b87933
mahbubme <mail@mahbub.me>
2016-02-21 12:46:53 +0600

    Relating Categories to posts and Displaying it

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -35,7 +35,17 @@
                 $output .= "<td>{$post_id}</td>";
                 $output .= "<td>{$post_author}</td>";
                 $output .= "<td>{$post_title}</td>";
-                $output .= "<td>{$post_category_id}</td>";
+
+                $query = "SELECT * FROM categories WHERE cat_id = {$post_category_id}";
+                $select_categories_id = mysqli_query( $connection, $query );
+
+                while ( $row = mysqli_fetch_assoc( $select_categories_id ) ) {
+                    $cat_id = $row['cat_id'];
+                    $cat_title = $row['cat_title'];
+                
+                    $output .= "<td>{$cat_title}</td>";
+                }
+
                 $output .= "<td>{$post_status}</td>";
                 $output .= "<td><img width='100' src='../images/{$post_image}' alt='image'></td>";
                 $output .= "<td>{$post_tags}</td>";
