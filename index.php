f0f1c9c865c64402c7b124cb7652320f8a9315da
mahbubme <mail@mahbub.me>
2016-02-22 23:25:39 +0600

    adding individual post page and link

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
                     $select_all_posts_query = mysqli_query( $connection, $query );
                     
                     while ( $row = mysqli_fetch_assoc( $select_all_posts_query ) ) {
+                        $post_id = $row['post_id'];
                         $post_title = $row['post_title'];
                         $post_author = $row['post_author'];
                         $post_date = $row['post_date'];
@@ -34,7 +35,7 @@
         
                         <!-- First Blog Post -->
                         <h2>
-                            <a href="#"><?php echo $post_title; ?></a>
+                            <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
                         </h2>
                         <p class="lead">
                             by <a href="index.php"><?php echo $post_author; ?></a>
@@ -44,7 +45,7 @@
                             <?php echo $post_date; ?>
                         </p>
                         <hr>
-                        <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+                        <a href="post.php?p_id=<?php echo $post_id; ?>"><img class="img-responsive" src="images/<?php echo $post_image; ?>" alt=""></a>
                         <hr>
                         <p><?php echo $post_content; ?></p>
                         <a class="btn btn-primary" href="#">
@@ -56,8 +57,6 @@
                         
                     
                     <?php } ?>
-
-                
 
                 
 
