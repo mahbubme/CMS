724536fafb1ba7c79cf02d1d8806e61a553ae55b
mahbubme <mail@mahbub.me>
2016-03-05 23:19:52 +0600

    creating the login form

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -16,6 +16,27 @@
         </form>
         <!--Search Form-->
     </div>
+
+    <!-- Login -->
+    <div class="well">
+        <h4>Login</h4>
+            <form action="includes/login.php" method="POST">
+                <div class="form-group">
+                    <input name="username" type="text" class="form-control" placeholder="Enter Username">
+                    <!-- /.form-group -->
+                </div>
+                <div class="input-group">
+                    <input name="password" type="password" class="form-control" placeholder="Enter Password">
+                    <span  class="input-group-btn" >
+                        <button class="btn btn-primary" name="login" type="submit">
+                            Submit
+                        </button>
+                    </span>
+                    <!-- /.input-group -->
+                </div>
+        </form>
+        <!--Search Form-->
+    </div>
     
     
     <?php 
