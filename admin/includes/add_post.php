8c6a1b15689f2752af7930176c00c30e15e25c01
mahbubme <mail@mahbub.me>
2016-03-11 00:47:08 +0600

    adding post creation notice to add post page

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -21,6 +21,10 @@
 		$create_post_query = mysqli_query( $connection, $query );
 
 		confirmQuery( $create_post_query );
+
+		$the_post_id = mysqli_insert_id( $connection);
+		
+		echo "<p class='bg-success'>Post Created. <a href='../post.php?p_id={$the_post_id}'>View Post</a> or <a href='posts.php'>Edit More Posts</a></p>";
 	}
 
 ?>
