e1bd5770b500c59d33417454b48318395fb0140a
mahbubme <mail@mahbub.me>
2016-03-12 23:22:00 +0600

    relating posts to their authors

--- author_posts.php
+++ author_posts.php
@@ -11,57 +11,52 @@
 
             <!-- Blog Entries Column -->
             <div class="col-md-8">
-                
+
                 <h1 class="page-header">
                     Page Heading
                     <small>Secondary Text</small>
                 </h1>
                 
-                <?php 
+                <?php
+
+                    if ( isset( $_GET['p_id'] ) ) {
+                        $the_post_id = $_GET['p_id'];
+                        $the_post_author = $_GET['author'];
+                    }
                     
-                    $query = "SELECT * FROM posts";
+                    $query = "SELECT * FROM posts WHERE post_author = '{$the_post_author}'";
                     $select_all_posts_query = mysqli_query( $connection, $query );
                     
                     while ( $row = mysqli_fetch_assoc( $select_all_posts_query ) ) {
-                        $post_id = $row['post_id'];
                         $post_title = $row['post_title'];
                         $post_author = $row['post_author'];
                         $post_date = $row['post_date'];
                         $post_image = $row['post_image'];
-                        $post_content = substr( $row['post_content'], 0,100 );
-                        $post_status = $row['post_status'];
-
-                        if ( $post_status == 'published' ) {
-                    
+                        $post_content = $row['post_content'];
+                        
                     ?>
-                    
-        
+                        
                         <!-- First Blog Post -->
                         <h2>
-                            <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
+                            <a href="#"><?php echo $post_title; ?></a>
                         </h2>
                         <p class="lead">
-                            by <a href="author_posts.php?author=<?php echo $post_author; ?>&p_id=<?php echo $post_id; ?>"><?php echo $post_author; ?></a>
+                            All post by <?php echo $post_author; ?>
                         </p>
                         <p>
                             <span class="glyphicon glyphicon-time"></span> 
                             <?php echo $post_date; ?>
                         </p>
                         <hr>
-                        <a href="post.php?p_id=<?php echo $post_id; ?>"><img class="img-responsive" src="images/<?php echo $post_image; ?>" alt=""></a>
+                        <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
                         <hr>
                         <p><?php echo $post_content; ?></p>
-                        <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">
-                            Read More 
-                            <span class="glyphicon glyphicon-chevron-right"></span>
-                        </a>
         
                         <hr>
                         
                     
-                    <?php } } ?>
+                    <?php } ?>
 
-                
             </div>
 
             <!-- Blog Sidebar Widgets Column -->
