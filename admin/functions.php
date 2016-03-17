a785d90b10149505d54640fff9f15c6d6951bff9
mahbubme <mail@mahbub.me>
2016-03-17 23:51:06 +0600

    count users online

--- admin/functions.php
+++ admin/functions.php
@@ -1,5 +1,34 @@
 <?php 
 
+// Users Online
+function users_online() {
+
+    global $connection;
+    
+    $session = session_id();
+    $time = time();
+    $time_out_in_seconds = 30;
+    $time_out = $time - $time_out_in_seconds;
+
+    $query = "SELECT * FROM users_online WHERE session = '$session'";
+    $send_query = mysqli_query( $connection, $query );
+    $count = mysqli_num_rows( $send_query );
+
+    if ( $count == NULL ) {
+
+        mysqli_query( $connection, "INSERT INTO users_online(session, time) VALUES('$session','$time')");    
+
+    }else{
+
+        mysqli_query( $connection, "UPDATE users_online SET time = '$time' WHERE session = '$session'");
+
+    }
+
+    $users_online_query = mysqli_query( $connection, "SELECT * FROM users_online WHERE time > '$time_out_in_seconds'");
+
+    return $count_user = mysqli_num_rows( $users_online_query );
+
+}
 
 // Confirm Database Query
 function confirmQuery( $result ) {
--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -11,6 +11,8 @@
     </div>
     <!-- Top Menu Items -->
     <ul class="nav navbar-right top-nav">
+        <li><a href="#">Users Online: <?php echo users_online(); ?></a></li>
+
         <li><a href="../index.php">HOME SITE</a></li>
         
         <li class="dropdown">
